477fc6475878410e44933cf5ed202af7fdf6a66e
ChristianMor <christian.d.morales@gmail.com>
2025-03-26 18:02:45 -0300

    Setting img size and connecting to Game

--- code/Game.py
+++ code/Game.py
@@ -2,12 +2,13 @@
 import pygame
 
 from code.Menu import Menu
+from code.const import WIN_WIDTH, WIN_HEIGHT
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
 
