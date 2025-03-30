2c4857749cad92795afa2ab672d6ed4de6529ae9
ChristianMor <christian.d.morales@gmail.com>
2025-03-30 17:18:14 -0300

    Creating Menu intarations Creating Abs class Creating Factory Background lvl 1 implementation: parallax

--- code/Game.py
+++ code/Game.py
@@ -1,8 +1,8 @@
-
 import pygame
 
+from code.Level import Level
 from code.Menu import Menu
-from code.const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 
 
 class Game:
@@ -13,12 +13,19 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[40]:
+                pygame.quit()  # close Window
+                quit()  # End pygame
+            else:
+                pass
 
             #  check for all events
             # for event in pygame.event.get():
             #     if event.type == pygame.QUIT:
             #         pygame.quit()  # Close window
             #         quit()  # End pygame
-
